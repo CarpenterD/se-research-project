9855a34624e3b0ed2069b6360d450c0bc4e52990
Dylan Carpenter <dsc170698@gmail.com>
2021-05-02 00:07:38 +0800

    #21 Add sample file and update config

---
+++ tests/samples/Conditions-1.c
@@ -0,0 +1,24 @@
+int main(int argc, char const *argv[])
+{
+    int x = 0;
+    if ( x == 6 ){
+
+    }
+
+    if ( 6 == x ){
+
+    }
+
+    char c = 'A';
+    if ( c == 'D' ){
+
+    }
+
+    double d = 1.0;
+    float f = d;
+    if ( d == 10.0 || "BIG" == "TEST"){
+
+    }
+
+    return 0;
+}
